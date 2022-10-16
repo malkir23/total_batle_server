b13705394772d34bd7cdf3f6ee87f8212d0efe57
malkir23 <mandragorand@gmail.com>
2022-10-17 01:33:25 +0300

    fix ip

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import socket
 
 def server_program():
     # get the hostname
-    host = socket.gethostname()
+    host = "127.0.0.1"
     port = 49152  # initiate port no above 1024
 
     server_socket = socket.socket()  # get instance
